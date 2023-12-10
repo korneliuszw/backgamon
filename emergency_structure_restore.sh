e2b7ab55e3f80ed69fd6ca427c097cf70697901f
korneliuszw <korneliuszwstudio@gmail.com>
2023-12-10 21:39:29 +0100

    Fixed script

--- emergency_structure_restore.sh
+++ emergency_structure_restore.sh
@@ -10,19 +10,24 @@ move_and_uppercase() {
     local destination_directory=$2
     for file in *.$source_extension; do
         filename=$(basename -- "$file" ".$source_extension")
-        new_filename="${filename^}"
-        echo $new_filename
+        new_filename="$(tr '[:lower:]' '[:upper:]' <<< ${filename:0:1})${filename:1}"
         mv "$file" "$destination_directory/$new_filename.$source_extension"
         echo "Moved $file to $destination_directory/$new_filename.$source_extension"
     done
 }
-
-# Move *.c files to directory 'src'
 move_and_uppercase "c" "src"
-
-# Copy *.h files to directory 'include'
 move_and_uppercase "h" "include"
 
-mv *.save saves/
-mv *.hist history/
+move () {
+    local source_extension=$1
+    local destination_directory=$2
+    for file in *.$source_extension; do
+        filename=$(basename -- "$file" ".$source_extension")
+        mv "$file" "$destination_directory/$file"
+        echo "Moved $file to $destination_directory/$file"
+    done
+}
+
+move save saves
+move hist history
 mv cmakelists.txt CMakeLists.txt
